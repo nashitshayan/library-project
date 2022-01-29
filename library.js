18e4e7e932f09b319d11efed7571d21017d82d50
nashitshayan <shayan.nashit@gmail.com>
2022-01-29 14:53:13 +0530

    feat: close btn for form

--- library.js
+++ library.js
@@ -1,21 +1,56 @@
 let myLibrary= [];
 
+const domElements= (()=>{
+    return {
+     bookCardsDiv : document.querySelector('.bookcards'),
+     addNewBookFormDiv :document.querySelector('.newBookFormWrapper'),
+     newBookForm : document.getElementById('newBookForm'),
+     closeNewBookForm: document.getElementById('closeNewBookForm'),
+    }
+})();
 
 
 
-const bookCardsDiv= document.querySelector('.bookcards');
-const deleteBookBtnList= document.querySelectorAll('#delete');
-const addNewBookFormDiv= document.querySelector('.newBookFormWrapper');
-const newBookForm= document.getElementById('newBookForm');
-const submitNewBookFormBtn= document.getElementById('submitForm');
-let readingStatus= document.querySelector('.bookReadStatus');
 
+
+
+//Book class
+class Book{
+
+    constructor(title,author,pages,isRead){
+        this.title= title;
+        this.author= author;
+        this.pages = pages + ' pages';
+        this.isRead= isRead? 'Read' : 'Not Read Yet';
+    }
+
+   static addBookToLibrary (...books){
+        books.forEach(book=>  myLibrary.push(book))
+    }
+    
+}
+
+
+
+//hard coding some sample books
+let b1= new Book('One Shot', 'Lee Child', 296, false);
+let b2= new Book('Harry Potter', 'J.K. Rowling', 560, false);
+let b3= new Book('Fellowship of the Ring', 'J.R.R. Tolkein', 690, false);
+let b4= new Book('The Two Towers', 'J.R.R. Tolkein', 450, false);
+let b5= new Book('The Return of The King', 'J.R.R. Tolkein', 750, false);
+let b6= new Book('Killing Floor', 'Lee Child', 296, false);
+
+Book.addBookToLibrary(b1,b2,b3,b4,b5,b6)
+
+
+
+// function to create and populate book cards
 
 const createNewBookCard= (title,author,pages,isRead)=>{
     //create the bookcard wrapper div and add to bookcards
     let bookCard= document.createElement('div');
     bookCard.classList.add("bookcard");
-    bookCardsDiv.appendChild(bookCard);
+    domElements.bookCardsDiv.appendChild(bookCard);
 
 
     // create and add the delete icon
@@ -83,37 +118,6 @@ const createNewBookCard= (title,author,pages,isRead)=>{
     bookCard.appendChild(updateReadStatusBtn);
  }
 
-
-
-//Book ctor
-class Book{
-
-    constructor(title,author,pages,isRead){
-        this.title= title;
-        this.author= author;
-        this.pages = pages + ' pages';
-        this.isRead= isRead? 'Read' : 'Not Read Yet';
-    }
-
-   static addBookToLibrary (...books){
-        books.forEach(book=>  myLibrary.push(book))
-    }
-    
-}
-
-
-
-//hard coding some sample books
-let b1= new Book('One Shot', 'Lee Child', 296, false);
-let b2= new Book('Harry Potter', 'J.K. Rowling', 560, false);
-let b3= new Book('Fellowship of the Ring', 'J.R.R. Tolkein', 690, false);
-let b4= new Book('The Two Towers', 'J.R.R. Tolkein', 450, false);
-let b5= new Book('The Return of The King', 'J.R.R. Tolkein', 750, false);
-let b6= new Book('Killing Floor', 'Lee Child', 296, false);
-
-Book.addBookToLibrary(b1,b2,b3,b4,b5,b6)
-
-
 //make a book card for each book in the library
 myLibrary.forEach(book => {
     createNewBookCard(book.title, book.author, book.pages, book.isRead);
@@ -121,14 +125,14 @@ myLibrary.forEach(book => {
 
 
 //event lister on the parent div for event delegation. 
-bookCardsDiv.addEventListener('click', (e)=>{
+domElements.bookCardsDiv.addEventListener('click', (e)=>{
     
     if(e.target.id==='delete')
-        bookCardsDiv.removeChild(e.target.parentElement);
+        domElements.bookCardsDiv.removeChild(e.target.parentElement);
     else if(e.target.id==='addNew')
         {
-            bookCardsDiv.style.display='none';
-            addNewBookFormDiv.style.display='flex';
+            domElements.bookCardsDiv.style.display='none';
+           domElements.addNewBookFormDiv.style.display='flex';
         }
     else if(e.target.id==='changeReadStatus')
         {   
@@ -142,7 +146,7 @@ bookCardsDiv.addEventListener('click', (e)=>{
 })
 
 //new Book Form submit
-newBookForm.addEventListener('submit', (e)=>{
+domElements.newBookForm.addEventListener('submit', (e)=>{
     e.preventDefault();
 
     const formData = new FormData(e.target);
@@ -156,8 +160,14 @@ newBookForm.addEventListener('submit', (e)=>{
                             isRead);
     Book.addBookToLibrary(newBook)
     createNewBookCard(newBook.title, newBook.author, newBook.pages, newBook.isRead)
-    newBookForm.reset();
-    bookCardsDiv.style.display='flex';
-    addNewBookFormDiv.style.display='none';
+    domElements.newBookForm.reset();
+    domElements.bookCardsDiv.style.display='flex';
+    domElements.addNewBookFormDiv.style.display='none';
     
+})
+
+//close form btn 
+domElements.closeNewBookForm.addEventListener('click', (e)=>{
+    domElements.bookCardsDiv.style.display='flex';
+    domElements.addNewBookFormDiv.style.display='none'; 
 })
